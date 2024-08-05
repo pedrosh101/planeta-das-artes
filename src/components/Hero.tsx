a8cd479e64a19b9ae7655791011a54c92fd5382d
pedrosh101 <pedro.sh101@gmail.com>
2024-08-05 11:41:38 -0300

    first-commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <header className="hero__header">
+      <h1>Planeta Das Artes</h1>
+      <h2 className="font-2xl red">Ateliê</h2>
+    </header>
+  );
+}
+
+export default Hero;
