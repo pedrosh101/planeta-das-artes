f280c3253e8b1c9189842aec6b285a3797608215
pedrosh101 <pedro.sh101@gmail.com>
2024-08-12 15:47:10 -0300

    withgliphs

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <header className="hero__header font-font2">
-      <h1 className="font-mainFont text-[9vw]">Planeta Das Artes</h1>
+      <h1 className="font-mainFont text-[9vw]">laneta Das rtes</h1>
       <h2 className="text-2xl text-white tracking-[20px]">ATELIÊ</h2>
     </header>
   );
